35ba08fb9d325f51a5456f1bb21d34d3ce4fb873
GusTheBusNG <ngustafson@sc.rr.com>
2016-08-28 15:24:04 -0400

    Event and Scout Controller

--- app/Http/Controllers/ScoutController.php
+++ app/Http/Controllers/ScoutController.php
@@ -8,5 +8,27 @@ use App\Http\Requests;
 
 class ScoutController extends Controller
 {
-    //
+    public function _construct() {
+        $this->middleware('auth');
+    }
+
+    public function index() {
+
+    }
+
+    public function create() {
+        //return a view that has a form with it
+    }
+
+    public function store(Request $request) {
+        //stores all the data from the create form
+    }
+
+    public function edit($id) {
+        //return a view with the id of the object the person wants to edit
+    }
+
+    public function update($id, Request $request) {
+        //stores all the new information from the edit form
+    }
 }
